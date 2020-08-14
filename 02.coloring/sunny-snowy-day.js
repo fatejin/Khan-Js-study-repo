df66d9d9e1bb3438ff45ec5df2d04a73f28fc85b
Park Byeong Jin <67573062+fatejin@users.noreply.github.com>
2020-08-14 16:20:16 +0900

    02.coloring

---
+++ 02.coloring/sunny-snowy-day.js
@@ -0,0 +1,4 @@
+// Back-ground-color background(0, 255, 217); // The ground fill(0, 255, 51);
+rect(0, 300, 400, 100); // The sun fill(255, 255, 0); ellipse(80, 64, 100, 100);
+// The snowman fill(255, 255, 255); ellipse(200, 300, 150, 150); ellipse(200,
+200, 100, 100); ellipse(200, 120, 75, 75);
